69e3121083fb86977a28d0c42c2082e1248ac783
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-12-10 23:47:33 -0800

    feat: Add button click event and grid size validation

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ const redPen = document.getElementById("red-pen");
 const bluePen = document.getElementById("blue-pen");
 const rainbowPen = document.getElementById("rainbow-pen");
 const eraser = document.getElementById("eraser");
+
+submitBtn.addEventListener("click", () => {
+  const gridSize = Number(userInput.value);
+
+  if (isNaN(gridSize)) {
+    alert("Please enter a number!");
+  } else if (gridSize < 1 || gridSize > 100) {
+    alert("Please enter a number between 1 and 100!");
+  } else {
+    console.log("User entered:", gridSize);
+
+    // Grid function start here
+  }
+});
