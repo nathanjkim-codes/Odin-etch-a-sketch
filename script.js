12a50842c559cf4b608be06531f4768dab5ca431
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-12-10 22:29:35 -0800

    refactor: update DOM element selectors to match new HTML structure

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const gridSizeInput = document.getElementById("gridSize");
-const gridSizeBtn = document.getElementById("gridSizeBtn");
-const container = document.getElementById("container");
-const container = document.getElementById("color-container");
+const userInput = document.getElementById("user-input");
+const submitBtn = document.getElementById("submitBtn");
+const gridContainer = document.getElementById("grid-container");
+const form = document.getElementById("form");
+const colorContainer = document.getElementById("color-container");
+
+const blackPen = document.getElementById("black-pen");
+const redPen = document.getElementById("red-pen");
+const bluePen = document.getElementById("blue-pen");
+const rainbowPen = document.getElementById("rainbow-pen");
+const eraser = document.getElementById("eraser");
