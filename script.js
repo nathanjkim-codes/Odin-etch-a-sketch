395b64888ee9576b52f020e785084d4e53b6e0da
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-12-10 20:53:23 -0800

    feat: add color container and radio button event handling

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const gridSizeInput = document.getElementById("gridSize");
 const gridSizeBtn = document.getElementById("gridSizeBtn");
 const container = document.getElementById("container");
+const container = document.getElementById("color-container");
