0f02fa15b9592ccac9ea78f8d25c94979a10df87
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-12-11 23:37:15 -0800

    refactor: Moved DOM selectors to feature branch and refined them

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const userInput = document.getElementById("user-input");
 const submitBtn = document.getElementById("submitBtn");
 const gridContainer = document.getElementById("grid-container");
-const form = document.getElementById("form");
-const colorContainer = document.getElementById("color-container");
-
-const blackPen = document.getElementById("black-pen");
-const redPen = document.getElementById("red-pen");
-const bluePen = document.getElementById("blue-pen");
-const rainbowPen = document.getElementById("rainbow-pen");
-const eraser = document.getElementById("eraser");
 
 submitBtn.addEventListener("click", () => {
   const gridSize = Number(userInput.value);
@@ -19,7 +11,5 @@ submitBtn.addEventListener("click", () => {
     alert("Please enter a number between 1 and 100!");
   } else {
     console.log("User entered:", gridSize);
-
-    // Grid function start here
   }
 });
